941ae3bd5bead68aab770dd69110a802ef1492a4
Cassandra <cparisi1290@gmail.com>
2020-12-27 11:33:20 -0500

    commit tree 81b9bd3fd148aaa5e8a29992e77cd290ea4e2017

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+    
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def deposit(deposit)
+        @balance = @balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+       status == "open" && balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
